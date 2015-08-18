bd4fa4549d38ab199bc0283d3b3f654c2cc9ee78
kwSeo <tkruddnjs1@naver.com>
2015-08-19 00:47:23 +0900

    hive 쿼리랑 Chicken 클래스 약간 수정. 지금 실행안되는 상태임

--- chicken-server/src/main/java/team/gtfm/server/bean/Chicken.java
+++ chicken-server/src/main/java/team/gtfm/server/bean/Chicken.java
@@ -7,136 +7,200 @@ public class Chicken {
 	private String bplcNm;
 	private String siteWhlAddr;
 	private String rdnWhlAddr;
-	private Date apvPermYmd;			
+	private Date apvPermYmd;
 	private String trdStateNm;
 	private int siteArea;
 	private int sitePostNo;
 	private String wtrSplyFacilSeNm;
 	private int manEipCnt;
 	private int yy;
-	private String multUsnUpsoYn; 
-	private String lvSeNm;			
-	private String facilTotScp;		
-	private int wmEipCnt;				
-	private String trdpJubnSeNm;		
-	private String sntCobNm;			
-	private String sntUptaeNm;		
+	private String multUsnUpsoYn;
+	private String lvSeNm;
+	private String facilTotScp;
+	private int wmEipCnt;
+	private String trdpJubnSeNm;
+	private String sntCobNm;
+	private String sntUptaeNm;
 	private int totEpNum;
+	private String memo;
+	private String kind;
+	private String kind_detail;
+
 	public int getRowNum() {
 		return rowNum;
 	}
+
 	public void setRowNum(int rowNum) {
 		this.rowNum = rowNum;
 	}
+
 	public String getBplcNm() {
 		return bplcNm;
 	}
+
 	public void setBplcNm(String bplcNm) {
 		this.bplcNm = bplcNm;
 	}
+
 	public String getSiteWhlAddr() {
 		return siteWhlAddr;
 	}
+
 	public void setSiteWhlAddr(String siteWhlAddr) {
 		this.siteWhlAddr = siteWhlAddr;
 	}
+
 	public String getRdnWhlAddr() {
 		return rdnWhlAddr;
 	}
+
 	public void setRdnWhlAddr(String rdnWhlAddr) {
 		this.rdnWhlAddr = rdnWhlAddr;
 	}
+
 	public Date getApvPermYmd() {
 		return apvPermYmd;
 	}
+
 	public void setApvPermYmd(Date apvPermYmd) {
 		this.apvPermYmd = apvPermYmd;
 	}
+
 	public String getTrdStateNm() {
 		return trdStateNm;
 	}
+
 	public void setTrdStateNm(String trdStateNm) {
 		this.trdStateNm = trdStateNm;
 	}
+
 	public int getSiteArea() {
 		return siteArea;
 	}
+
 	public void setSiteArea(int siteArea) {
 		this.siteArea = siteArea;
 	}
+
 	public int getSitePostNo() {
 		return sitePostNo;
 	}
+
 	public void setSitePostNo(int sitePostNo) {
 		this.sitePostNo = sitePostNo;
 	}
+
 	public String getWtrSplyFacilSeNm() {
 		return wtrSplyFacilSeNm;
 	}
+
 	public void setWtrSplyFacilSeNm(String wtrSplyFacilSeNm) {
 		this.wtrSplyFacilSeNm = wtrSplyFacilSeNm;
 	}
+
 	public int getManEipCnt() {
 		return manEipCnt;
 	}
+
 	public void setManEipCnt(int manEipCnt) {
 		this.manEipCnt = manEipCnt;
 	}
+
 	public int getYy() {
 		return yy;
 	}
+
 	public void setYy(int yy) {
 		this.yy = yy;
 	}
+
 	public String getMultUsnUpsoYn() {
 		return multUsnUpsoYn;
 	}
+
 	public void setMultUsnUpsoYn(String multUsnUpsoYn) {
 		this.multUsnUpsoYn = multUsnUpsoYn;
 	}
+
 	public String getLvSeNm() {
 		return lvSeNm;
 	}
+
 	public void setLvSeNm(String lvSeNm) {
 		this.lvSeNm = lvSeNm;
 	}
+
 	public String getFacilTotScp() {
 		return facilTotScp;
 	}
+
 	public void setFacilTotScp(String facilTotScp) {
 		this.facilTotScp = facilTotScp;
 	}
+
 	public int getWmEipCnt() {
 		return wmEipCnt;
 	}
+
 	public void setWmEipCnt(int wmEipCnt) {
 		this.wmEipCnt = wmEipCnt;
 	}
+
 	public String getTrdpJubnSeNm() {
 		return trdpJubnSeNm;
 	}
+
 	public void setTrdpJubnSeNm(String trdpJubnSeNm) {
 		this.trdpJubnSeNm = trdpJubnSeNm;
 	}
+
 	public String getSntCobNm() {
 		return sntCobNm;
 	}
+
 	public void setSntCobNm(String sntCobNm) {
 		this.sntCobNm = sntCobNm;
 	}
+
 	public String getSntUptaeNm() {
 		return sntUptaeNm;
 	}
+
 	public void setSntUptaeNm(String sntUptaeNm) {
 		this.sntUptaeNm = sntUptaeNm;
 	}
+
 	public int getTotEpNum() {
 		return totEpNum;
 	}
+
 	public void setTotEpNum(int totEpNum) {
 		this.totEpNum = totEpNum;
 	}
-	
-	
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+
+	public String getKind_detail() {
+		return kind_detail;
+	}
+
+	public void setKind_detail(String kind_detail) {
+		this.kind_detail = kind_detail;
+	}
+
 	
 }
